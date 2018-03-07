41a9b2f1d6e49d1402dc9171e26a963078200e5c
Onur Gumus <onur.gumus@emaratech.ae>
2018-03-07 18:53:13 +0400

    Add project files.

---
+++ TradingDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TradingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
